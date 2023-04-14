a2fd1c6179de2498879e8e68a49c474394abf85c
ep-aotto <aotto@epages.com>
2023-04-14 18:01:34 +0200

    inital commit

---
+++ src/main/java/com/freundebuchag/freund/FreundApplication.java
@@ -0,0 +1,13 @@
+package com.freundebuchag.freund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freundebuchag/freund/FreundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freundebuchag.freund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
